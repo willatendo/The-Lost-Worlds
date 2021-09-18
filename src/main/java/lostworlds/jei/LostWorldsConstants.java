ff77c842a74c1d85407dcd62f99b0f1411a3769e
Willatendo <williamjswindler@gmail.com>
2021-09-18 11:41:27 -0600

    Update LostWorldsConstants.java

--- src/main/java/lostworlds/jei/LostWorldsConstants.java
+++ src/main/java/lostworlds/jei/LostWorldsConstants.java
@@ -3,8 +3,29 @@ package lostworlds.jei;
 import lostworlds.library.util.ModUtils;
 import net.minecraft.util.ResourceLocation;
 
-public class LostWorldsConstants 
+public class LostWorldsConstants
 {
+	public static final ResourceLocation FOSSIL_CLEANER_PLUGIN = ModUtils.rL("fossil_cleaner_plugin");
+	public static final ResourceLocation FOSSIL_CLEANER_CATEGORY = ModUtils.rL("fossil_cleaner_category");
+		
+	public static final ResourceLocation FOSSIL_GRINDER_PLUGIN = ModUtils.rL("fossil_grinder_plugin");
+	public static final ResourceLocation FOSSIL_GRINDER_CATEGORY = ModUtils.rL("fossil_grinder_category");
+		
+	public static final ResourceLocation DNA_EXTRACTOR_PLUGIN = ModUtils.rL("dna_extractor_plugin");
+	public static final ResourceLocation DNA_EXTRACTOR_CATEGORY = ModUtils.rL("dna_extractor_category");
+	
+	public static final ResourceLocation ANALYSER_PLUGIN = ModUtils.rL("analyser_plugin");
+	public static final ResourceLocation ANALYSER_CATEGORY = ModUtils.rL("analyser_category");
+	
+	public static final ResourceLocation DNA_INJECTOR_PLUGIN = ModUtils.rL("dna_injector_plugin");
+	public static final ResourceLocation DNA_INJECTOR_CATEGORY = ModUtils.rL("dna_injector_category");
+	
+	public static final ResourceLocation CULTIVATOR_PLUGIN = ModUtils.rL("cultivator_plugin");
+	public static final ResourceLocation CULTIVATOR_CATEGORY = ModUtils.rL("cultivator_category");
+	
+	public static final ResourceLocation KILN_PLUGIN = ModUtils.rL("kiln_plugin");
+	public static final ResourceLocation KILN_CATEGORY = ModUtils.rL("kiln_category");
+	
 	public static final ResourceLocation TIME_MACHINE_PLUGIN = ModUtils.rL("time_machine_plugin");
 	public static final ResourceLocation TIME_MACHINE_CATEGORY = ModUtils.rL("time_machine_category");
 }
