f84b79a3550b0f60ca5fa72687cb38edd45b7cf8
Willatendo <williamjswindler@gmail.com>
2021-11-01 12:37:15 -0600

    Rivers

--- src/main/java/lostworlds/library/dimension/permian/layer/PermianRiverLayer.java
+++ src/main/java/lostworlds/library/dimension/permian/layer/PermianRiverLayer.java
@@ -5,36 +5,18 @@ import lostworlds.library.dimension.permian.PermianLayerUtil;
 import net.minecraft.world.gen.INoiseRandom;
 import net.minecraft.world.gen.layer.traits.ICastleTransformer;
 
-public enum PermianRiverLayer implements ICastleTransformer 
+public class PermianRiverLayer implements ICastleTransformer 
 {
-	INSTANCE;
-
-	PermianRiverLayer() { }
+	public PermianRiverLayer() { }
 
 	@Override
-	public int apply(INoiseRandom random, int north, int west, int south, int east, int center) 
+	public int apply(INoiseRandom random, int north, int east, int south, int west, int center) 
 	{
-		if(shouldRiver(center, west, south, east, north)) 
+		if(center != north || center != east || center != south || center != west) 
 		{
 			return PermianLayerUtil.getBiomeId(BiomeKeys.PERMIAN_RIVER);
-		} 
-		else 
-		{
-			return -1;
 		}
-	}
 
-	boolean shouldRiver(int mid, int left, int down, int right, int up) 
-	{
-		return shouldRiver(mid, left) || shouldRiver(mid, right) || shouldRiver(mid, down) || shouldRiver(mid, up);
-	}
-
-	boolean shouldRiver(int id1, int id2) 
-	{
-		if(id1 == id2)
-			return false;
-		if(id1 == id2)
-			return false;
-		return true;
+		return -1;
 	}
 }
--- src/main/java/lostworlds/library/dimension/permian/layer/PermianRiverMixLayer.java
+++ src/main/java/lostworlds/library/dimension/permian/layer/PermianRiverMixLayer.java
@@ -1,30 +1,28 @@
 package lostworlds.library.dimension.permian.layer;
 
-import lostworlds.library.biome.BiomeKeys;
 import lostworlds.library.dimension.permian.PermianLayerUtil;
 import net.minecraft.world.gen.INoiseRandom;
 import net.minecraft.world.gen.area.IArea;
 import net.minecraft.world.gen.layer.traits.IAreaTransformer2;
 import net.minecraft.world.gen.layer.traits.IDimOffset0Transformer;
 
-public enum PermianRiverMixLayer implements IAreaTransformer2, IDimOffset0Transformer
+public class PermianRiverMixLayer implements IAreaTransformer2, IDimOffset0Transformer 
 {
-	INSTANCE;
-
-	PermianRiverMixLayer() { }
+	public PermianRiverMixLayer() { }
 
 	@Override
-	public int applyPixel(INoiseRandom random, IArea area1, IArea area2, int val1, int val2) 
+	public int applyPixel(INoiseRandom random, IArea parent1, IArea parent2, int x, int y) 
 	{
-		int i = area1.get(this.getParentX(val1), this.getParentY(val2));
-		int j = area2.get(this.getParentX(val1), this.getParentY(val2));
-		if(j == PermianLayerUtil.getBiomeId(BiomeKeys.PERMIAN_RIVER)) 
+		final int biome = parent1.get(getParentX(x), getParentY(y));
+		final int river = parent2.get(getParentX(x), getParentY(y));
+
+		if(!PermianLayerUtil.isOcean(biome)) 
 		{
-			return j;
-		} 
-		else 
-		{
-			return i;
+			if(PermianLayerUtil.isRiver(river)) 
+			{
+				return river;
+			}
 		}
+		return biome;
 	}
 }
