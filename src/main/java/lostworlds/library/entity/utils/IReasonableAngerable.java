a6cf7ca250a22a7e6e7855045206b319df7bb89b
Willatendo <williamjswindler@gmail.com>
2022-01-28 22:49:51 -0700

    Update IReasonableAngerable.java

--- src/main/java/lostworlds/library/entity/utils/IReasonableAngerable.java
+++ src/main/java/lostworlds/library/entity/utils/IReasonableAngerable.java
@@ -8,17 +8,8 @@ import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 
 public interface IReasonableAngerable extends IAngerable {
-//	default boolean isHungry(LivingEntity entity) {
-//		if (entity instanceof PrehistoricEntity) {
-//			if (((PrehistoricEntity) entity).isHungry()) {
-//				return true;
-//			}
-//		} else {
-//			return false;
-//		}
-//		return false;
-//	}
-
+	
+	//To Move To New Class
 	public class ReasonedAttackableTargetGoal<T extends LivingEntity> extends NearestAttackableTargetGoal<T> {
 		public ReasonedAttackableTargetGoal(MobEntity entity, Class targetClass, Predicate<LivingEntity> reason) {
 			super(entity, targetClass, 10, true, false, reason);
